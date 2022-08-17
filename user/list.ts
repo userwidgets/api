e1044d6bf006fd2b86c9b8ee5bcd0039f46214cb
Elias Eriksson <eliaseriksson95@gmail.com>
2022-08-17 15:27:34 +0200

    header not needed for listing

--- user/list.ts
+++ user/list.ts
@@ -6,23 +6,30 @@ import { router } from "../router"
 
 export async function list(request: http.Request, context: Context): Promise<http.Response.Like | any> {
 	let result: model.User[] | gracely.Error
+	let application: string | undefined | string[]
 	const key = await context.authenticator.authenticate(request, "token", "admin")
 	if (gracely.Error.is(context.storage.user))
 		result = context.storage.user
 	else if (!key)
 		result = gracely.client.unauthorized()
-	else if (!request.header.application)
+	else if (
+		!(application = request.header.application ?? (key != undefined && key != "admin" ? key.audience : undefined))
+	)
 		result = gracely.client.missingHeader("Application", "Must include Application for this resource.")
-	else if (typeof request.header.application != "string")
+	else if (typeof application != "string")
 		result = gracely.client.malformedHeader("Application", "expected Application value to be a string.")
 	else
 		result =
-			key != "admin" && key.audience != request.header.application
+			key != "admin" && key.audience != application
 				? gracely.client.unauthorized("forbidden")
 				: key == "admin" || key.permissions["*"].user.read
-				? await context.storage.user.list(request.header.application)
+				? request.search.organizationId
+					? await context.storage.user.list(application, [request.search.organizationId])
+					: await context.storage.user.list(application)
+				: request.search.organizationId && key.permissions[request.search.organizationId]?.user?.read
+				? await context.storage.user.list(application, [request.search.organizationId])
 				: await context.storage.user.list(
-						request.header.application,
+						application,
 						Object.entries(key.permissions).reduce((organizationIds, [organizationId, organization]) => {
 							if (organizationId != "*" && organization?.user.read)
 								organizationIds.push(organizationId)

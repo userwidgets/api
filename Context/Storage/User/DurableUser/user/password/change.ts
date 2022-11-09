901faf579a36804ceb7024239b1a9158902c82f6
Linus Johansson <linus@issuefab.com>
2022-11-09 13:48:25 +0100

    change password end point

--- Context/Storage/User/DurableUser/user/password/change.ts
+++ Context/Storage/User/DurableUser/user/password/change.ts
@@ -14,16 +14,22 @@ export async function change(request: http.Request, context: Context): Promise<g
 		result = gracely.client.malformedContent(
 			"User.Password.Change",
 			"User.Password.Change",
+			"A User.Password.Change object is required to change a users password."
+		)
+	else if (!model.User.Password.Change.validate(passwords))
+		result = gracely.client.malformedContent(
+			"User.Password.Change",
+			"User.Password.Change",
 			"A valid User.Password.Change object is required to change a users password."
 		)
 	else if (!current)
 		result = gracely.client.notFound("Requested user does not exist.")
 	else {
-		const verifiedPassword = await password.verify(passwords.old, current, context.environment.hashSecret)
-		if (!verifiedPassword)
-			result = gracely.client.invalidContent("password", "Old password does not match current password.")
-		else if (gracely.Error.is(verifiedPassword))
+		const verifiedPassword = await password.verify(passwords.old, current, context.environment.hashSecret) //correct
+		if (gracely.Error.is(verifiedPassword))
 			result = verifiedPassword
+		else if (!verifiedPassword)
+			result = gracely.client.invalidContent("password", "Old password does not match current password.")
 		else {
 			const passwordHash = await password.hash(passwords.new, context.environment.hashSecret)
 			if (gracely.Error.is(passwordHash))

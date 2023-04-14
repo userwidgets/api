a4ceba8f440b190c41ded595282da09299207393
Elias Eriksson <eliaseriksson95@gmail.com>
2023-04-14 11:47:41 +0200

    application admin auth

--- application/fetch.ts
+++ application/fetch.ts
@@ -8,13 +8,15 @@ export async function fetch(request: http.Request, context: Context): Promise<ht
 	let result: model.Application | gracely.Error
 	const key = gracely.Error.is(context.authenticator)
 		? context.authenticator
-		: await context.authenticator.authenticate(request, "token")
+		: await context.authenticator.authenticate(request, "token", "admin")
 	if (gracely.Error.is(context.applications))
 		result = context.applications
 	else if (!key)
 		result = gracely.client.unauthorized()
 	else if (gracely.Error.is(key))
 		result = key
+	else if (key == "admin")
+		result = await context.applications.fetch()
 	else
 		(result = await context.applications.fetch()) &&
 			!gracely.Error.is(result) &&

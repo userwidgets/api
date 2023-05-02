c62c8589bbe584550a7de05ff5fe5f3ea992b9b2
Elias Eriksson <eliaseriksson95@gmail.com>
2023-05-02 16:30:03 +0200

    email endpoint

---
+++ email/create.ts
@@ -0,0 +1,40 @@
+import * as gracely from "gracely"
+import * as http from "cloudly-http"
+import { Context } from "../Context"
+import { router } from "../router"
+
+interface Email {
+	recipient: string
+	subject: string
+	content: string
+}
+namespace Email {
+	export function is(value: Email | any): value is Email {
+		return (
+			typeof value == "object" &&
+			value &&
+			typeof value.recipient == "string" &&
+			typeof value.subject == "string" &&
+			typeof value.content == "string"
+		)
+	}
+}
+
+export async function create(request: http.Request, context: Context): Promise<http.Response.Like | any> {
+	let result: gracely.Error | Awaited<ReturnType<typeof context.email>>
+	const authorization = gracely.Error.is(context.authenticator)
+		? context.authenticator
+		: await context.authenticator.authenticate(request, "admin")
+	const body: Email | unknown = await request.body
+	if (gracely.Error.is(authorization))
+		result = authorization
+	else if (authorization != "admin")
+		result = gracely.client.unauthorized()
+	else if (!Email.is(body))
+		result = gracely.client.malformedContent("Email", "Email", "Body is missing either: subject, recipient or content.")
+	else {
+		result = await context.email(body.recipient, body.subject, body.content)
+	}
+	return result
+}
+router.add("POST", "/email", create)

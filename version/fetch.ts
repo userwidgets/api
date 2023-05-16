cea056f0a7772336d49565db425e066bcab7e314
Lasse Löfquist <github@tvartom.com>
2023-05-16 15:30:51 +0200

    version fetch format

--- version/fetch.ts
+++ version/fetch.ts
@@ -10,9 +10,13 @@ export async function fetch(request: http.Request, context: Context): Promise<ht
 		const signer = Signer.create("RSA", "SHA-256", context.environment.publicKey, context.environment.privateKey)
 		if (await signer.verify("abc", await signer.sign("abc"))) {
 			publicKey = context.environment.publicKey
-			if ((request.search.keyFormat ?? "").toLowerCase() == "pem") {
-				publicKey =
-					"-----BEGIN PUBLIC KEY-----" + publicKey /*.replace(/(.{64})/g, "$1\n")*/ + "-----END PUBLIC KEY-----"
+			switch ((request.search.keyFormat ?? "").toLowerCase()) {
+				case "pem":
+					publicKey =
+						"-----BEGIN PUBLIC KEY-----" + publicKey.replace(/(.{64})/g, "$1\n") + "\n-----END PUBLIC KEY-----"
+					break
+				case "pem-one-line":
+					publicKey = "-----BEGIN PUBLIC KEY-----" + publicKey + "-----END PUBLIC KEY-----"
 			}
 		} else {
 			publicKey = "Misconfiguration: public- and privatekey mismatch."
@@ -30,8 +34,18 @@ export async function fetch(request: http.Request, context: Context): Promise<ht
 		!gracely.Error.is(context.authenticator) &&
 		(await context.authenticator.authenticate(request, "admin")) == "admin"
 	) {
-		// TODO: Corrupt secrets:
-		result.environment = context.environment
+		result.environment = {
+			...context.environment,
+			privateKey: context.environment.privateKey
+				? context.environment.privateKey.substring(0, 10) + "..." + context.environment.privateKey.slice(-10)
+				: "Missing!",
+			adminSecret: context.environment.adminSecret ? "****" : "Missing!",
+			hashSecret: context.environment.hashSecret ? "****" : "Missing!",
+			dkimPrivateKey: context.environment.dkimPrivateKey
+				? context.environment.dkimPrivateKey.substring(0, 10) + "..." + context.environment.dkimPrivateKey.slice(-10)
+				: "Missing!",
+		}
+
 		result.dependencies = data.dependencies
 	}
 

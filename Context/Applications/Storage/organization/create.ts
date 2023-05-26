aa17fee954082db2915715bd7b036fd8d6269642
Lasse Löfquist <tvartom@users.noreply.github.com>
2023-05-26 13:06:26 +0200

    Organization.creatable may have id. (#48)

--- Context/Applications/Storage/organization/create.ts
+++ Context/Applications/Storage/organization/create.ts
@@ -12,30 +12,32 @@ export async function create(
 	retries = 5
 ): Promise<model.Organization | gracely.Error> {
 	let result: model.Organization | gracely.Error
-	const organization: (model.Organization.Creatable & Record<"id", string | undefined>) | any = await request.body
-	const id = cryptly.Identifier.generate(8)
-	const current = await context.state.storage.get<model.Application>("data")
+	const organization: model.Organization.Creatable = await request.body
 	if (!model.Organization.Creatable.is(organization))
 		result = gracely.client.malformedContent(
 			"Organization.Creatable",
 			"Organization.Creatable",
 			"A valid Organization.Creatable object is required to create a new application."
 		)
-	else if (!current)
-		result = gracely.client.notFound("Application with this id does not exist.")
-	else if (current.organizations[id] && retries > 0)
-		result = await create(request, context, --retries)
 	else {
-		const now = isoly.DateTime.now()
-		current.organizations[id] = result = {
-			name: organization.name,
-			permissions: organization.permissions,
-			id: id,
-			created: now,
-			modified: now,
-			users: organization.users.map(({ email }) => email),
+		const id = organization.id ?? cryptly.Identifier.generate(8)
+		const current = await context.state.storage.get<model.Application>("data")
+		if (!current)
+			result = gracely.client.notFound("Application with this id does not exist.")
+		else if (current.organizations[id] && retries > 0)
+			result = await create(request, context, --retries)
+		else {
+			const now = isoly.DateTime.now()
+			current.organizations[id] = result = {
+				name: organization.name,
+				permissions: organization.permissions,
+				id,
+				created: now,
+				modified: now,
+				users: organization.users.map(({ email }) => email),
+			}
+			await context.state.storage.put<model.Application>("data", current)
 		}
-		await context.state.storage.put<model.Application>("data", current)
 	}
 	return result
 }

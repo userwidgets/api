91389d91e17434bf0c878bc803fbe2a6b43cd1d1
Elias Eriksson <eliaseriksson95@gmail.com>
2023-08-17 13:01:47 +0200

    updated error

--- Context/Applications/Storage/organization/list.ts
+++ Context/Applications/Storage/organization/list.ts
@@ -5,7 +5,7 @@ import { Context } from "../Context"
 import { router } from "../router"
 
 export async function list(_: http.Request, context: Context): Promise<userwidgets.Organization[] | gracely.Error> {
-	return (await context.organizations.list()) ?? []
+	return (await context.organizations.list()) ?? gracely.client.notFound("Organization not found.")
 }
 
 router.add("GET", "/organization", list)

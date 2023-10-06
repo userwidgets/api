842940f02a5126935888bda96aa9868b1be641ec
Linus Johansson <linus@issuefab.com>
2023-10-06 10:34:58 +0200

    updated imports

--- me/fetch.ts
+++ me/fetch.ts
@@ -1,6 +1,6 @@
-import * as gracely from "gracely"
-import * as authly from "authly"
-import * as http from "cloudly-http"
+import { gracely } from "gracely"
+import { authly } from "authly"
+import { http } from "cloudly-http"
 import { Context } from "../Context"
 import { router } from "../router"
 

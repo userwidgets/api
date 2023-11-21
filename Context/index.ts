86ec7fb3321bc7edad1221f78ff325f8b0f02e61
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-11-21 13:30:16 +0100

    Support both url and name.

--- Context/index.ts
+++ Context/index.ts
@@ -13,7 +13,7 @@ export class Context {
 	get referer(): string | undefined {
 		return (this.#referer ??=
 			typeof this.request.header.application == "string"
-				? this.request.header.application
+				? common.url.parse(this.request.header.application)?.hostname ?? this.request.header.application
 				: common.url.parse(this.request.header.referer)?.hostname ?? this.request.header.referer)
 	}
 	#applications?: Applications | gracely.Error

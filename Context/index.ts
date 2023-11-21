87335a1d1dbc60941a4f6685e3e4101ec3efc593
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-11-21 13:27:14 +0100

    Add application header.

--- Context/index.ts
+++ Context/index.ts
@@ -11,7 +11,10 @@ import { Users } from "./Users"
 export class Context {
 	#referer?: string
 	get referer(): string | undefined {
-		return (this.#referer ??= common.url.parse(this.request.header.referer)?.hostname ?? this.request.header.referer)
+		return (this.#referer ??=
+			typeof this.request.header.application == "string"
+				? this.request.header.application
+				: common.url.parse(this.request.header.referer)?.hostname ?? this.request.header.referer)
 	}
 	#applications?: Applications | gracely.Error
 	get applications(): Applications | gracely.Error {
